e2307ed3c00280a6dab455653f2f6b8e59097a07
jprolejko <jprolejko@gmail.com>
2019-11-24 19:21:48 +0100

    Simple tests script added

---
+++ run_tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+
+function check()
+{
+    for f in $(find tests/$1 -type f -name "*.lat")
+    do
+        echo "$f:"
+        ./latc_x86 -s $f >> /dev/null
+        if [[ $? -eq $2 ]] ; then
+            echo -e "\033[31mERROR!\e[0m"
+        else
+            echo -e "\e[92mOK!\e[0m"
+        fi
+    done
+}
+
+echo "BAD TESTS CHECK:"
+check "bad" 0
+
+echo -e "\n\nCORE TESTS CHECK:"
+check "good" 1
+
+echo -e "\n\nEXTENSIONS TESTS CHECK:"
+check "extensions" 1
